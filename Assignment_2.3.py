82442e441834a58240ba69227e5112f51311e5f9
Steven <Stevennesco@gmail.com>
2016-06-07 17:28:22 +0200

    Add Assignment 2.3

---
+++ Assignment_2.3.py
@@ -0,0 +1,12 @@
+#Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay. 
+#Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). 
+#You should use raw_input to read a string and float() to convert the string to a number. 
+#Do not worry about error checking or bad user data.
+#Desired Output:
+#96.25
+
+hrs = raw_input("Enter Hours:")
+hrs=float(hrs)
+rate = raw_input("Enter Rate:")
+rate=float(rate)
+print hrs*rate
