5dbf0f0100419522888f7b7958ec67eb7e516cae
Steven <Stevennesco@gmail.com>
2016-06-12 15:12:21 +0200

    Add Assignment 9.4

---
+++ Assignment_9.4.py
@@ -0,0 +1,22 @@
+name = raw_input("Enter file:")
+if len(name) < 1 :
+	name = "mbox-short.txt"
+fhand = open(name)
+maildict = {}
+
+for line in fhand:
+	if line.startswith("From "):
+		lst=line.split()
+		mail=lst[1]
+		maildict[mail] = maildict.get(mail,0) + 1
+
+
+greatestmail = 0
+greatestcount = 0
+
+for mail,count in maildict.items():
+	if greatestcount is None or count > greatestcount:
+		greatestmail = mail
+		greatestcount = count
+
+print greatestmail,greatestcount
